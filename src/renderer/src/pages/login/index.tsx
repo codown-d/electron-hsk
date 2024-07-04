62ae5673bc4ab16a98dcb19d80f9935a1a2b9715
DESKTOP-SQL335Q\zhanghe <123456@qq.com>
2024-07-04 18:31:01 +0800

    修改微隔离bug

--- src/renderer/src/pages/login/index.tsx
+++ src/renderer/src/pages/login/index.tsx
@@ -1,16 +1,21 @@
 import { useNavigate } from 'react-router-dom'
 import { Button, Form, Input } from 'antd'
 import electronLogo from '../../assets/electron.svg'
+import { useMemoizedFn } from 'ahooks'
 import './index.less'
 
 function Login() {
   // 创建路由钩子
   const navigate = useNavigate()
   const [form] = Form.useForm()
+  const onFinish = useMemoizedFn(() => {
+    navigate('/home')
+  })
   return (
     <div className="login-screen">
       <div className="login-screen-card">
         <Form form={form}>
+          <img alt="logo" className="logo" src={electronLogo} />
           <Form.Item name="note">
             <Input placeholder="账号" />
           </Form.Item>
@@ -18,13 +23,7 @@ function Login() {
             <Input.Password placeholder="密码" />
           </Form.Item>
         </Form>
-        <Button
-          type="primary"
-          block={true}
-          onClick={() => {
-            navigate('/home')
-          }}
-        >
+        <Button type="primary" block={true} onClick={onFinish}>
           登录
         </Button>
       </div>
